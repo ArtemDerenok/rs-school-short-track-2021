0c5e3e30d1b8d5d77ebb1817f2997f596f367e76
Artem Derenok <derenokartem95@gmail.com>
2021-04-11 19:35:44 +0300

    feat: add task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let str1 = '';
+  let cnt = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      cnt++;
+    } else {
+      cnt++;
+      if (cnt === 1) {
+        str1 += `${str[i]}`;
+      } else {
+        str1 += `${cnt}${str[i]}`;
+      }
+      cnt = 0;
+    }
+  }
+  return str1;
 }
 
 module.exports = encodeLine;
